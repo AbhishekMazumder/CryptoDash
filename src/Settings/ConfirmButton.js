222f9bb130f4844eb43977c60e971b7575c35779
abhishek <abhishekmazumder69@gmail.com>
2021-02-11 19:47:01 +0530

    Tile for CoinGrid added

--- src/Settings/ConfirmButton.js
+++ src/Settings/ConfirmButton.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 import { AppContext } from '../App/AppProvider';
+import { fontSize2, greenBoxShadow, color3 } from '../Shared/Styles';
 
 const ConfirmButtonStyled = styled.div`
 	margin: 20px;
-  color: green;
-  border: 3px green solid;
-  padding: 1rem 2rem;
-  cursor:pointer;
+	color: ${color3};
+	${fontSize2};
+	border: 3px green solid;
+	padding:10px 15px;
+	cursor: pointer;
+	border-radius: 60px;
+	&:hover {
+		${greenBoxShadow};
+		transition: all 0.3s linear;
+	}
 `;
 
 export const CenterDiv = styled.div`
