55e06ee2e165d5dcd51327ecdd670818083e74c2
abhishek <abhishekmazumder69@gmail.com>
2021-02-11 12:58:10 +0530

    AppBar Added

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import AppLayout from './AppLayout'
+import AppBar from './AppBar';
 
 function App() {
   return (
-    <AppLayout>
-      Hello World
-    </AppLayout>
-  );
+		<AppLayout>
+			<AppBar />
+			<h1>Welcome To CryptoDash</h1>
+		</AppLayout>
+	);
 }
 
 export default App;
