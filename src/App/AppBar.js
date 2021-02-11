ee754f08bb12137c78df91a6fe2b27f3b197265c
abhishek <abhishekmazumder69@gmail.com>
2021-02-11 13:33:17 +0530

    styling added to to AppBar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,18 +1,44 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const Logo = styled.div`
+	font-size: 2.5em;
+	font-family: 'Do Hyeon', sans-serif;
+`;
 
 const Bar = styled.div`
 	display: grid;
 	grid-template-columns: 180px auto 100px 100px;
+	margin-bottom: 40px;
+	align-items: center;
 `;
+
+const ControlButtonElem = styled.div`
+	cursor: pointer;
+	${props =>
+		props.active &&
+		css`
+			color: blue;
+			font-weight: bolder;
+			text-shadow: 0px 0px 60px #03ff03;
+		`}
+`;
+
+function toProperCase(lower) {
+	return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
+
+function ControlButton({ name, active }) {
+	return <ControlButtonElem active={active}>{toProperCase(name)}</ControlButtonElem>;
+}
 
 function AppBar() {
 	return (
 		<Bar>
-			<div>CryptoDash</div>
-      <div></div>
-			<div>Dashboard</div>
-			<div>Settings</div>
+			<Logo>CryptoDash</Logo>
+			<div></div>
+			<ControlButton active name="dashBoard" />
+			<ControlButton name="settings" />
 		</Bar>
 	);
 }
